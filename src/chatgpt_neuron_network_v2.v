2539bde590cc073da38a0e3f2eac2f2710d1c634
Michael Tomlinson <mtomlin5@jhu.edu>
2023-11-02 11:15:08 -0400

    Replaced manual changes with chatgpt fixes.

--- src/chatgpt_neuron_network_v2.v
+++ src/chatgpt_neuron_network_v2.v
@@ -11,7 +11,8 @@ module chatgpt_neuron_network (
     reg [7:0] THRESHOLD, LEAK_RATE, REFRAC_PERIOD;
     reg [7:0] SECOND_LAYER_WEIGHTS[2:0][2:0];
     reg [7:0] FIRST_LAYER_WEIGHTS[2:0];  // Weights for each neuron in the first layer
-    integer i, j, a, b, c;  // Loop variables
+    integer idx1, idx2, idx3, idx4, idx5, idx6, idx7;  // Loop variables
+    integer i, j; 
     reg [7:0] input_currents[2:0];  // Computed input currents for the first layer
 
     // Process data_in based on addr input
@@ -20,10 +21,10 @@ module chatgpt_neuron_network (
             THRESHOLD <= 8'd255;
             LEAK_RATE <= 8'd0;
             REFRAC_PERIOD <= 8'd0;
-            for (i = 0; i < 3; i = i + 1) begin
-                FIRST_LAYER_WEIGHTS[i] <= 8'd0;
-                for (j = 0; j < 3; j = j + 1) begin
-                    SECOND_LAYER_WEIGHTS[i][j] <= 8'd0;
+            for (idx1 = 0; idx1 < 3; idx1 = idx1 + 1) begin
+                FIRST_LAYER_WEIGHTS[idx1] <= 8'd0;
+                for (idx2 = 0; idx2 < 3; idx2 = idx2 + 1) begin
+                    SECOND_LAYER_WEIGHTS[idx1][idx2] <= 8'd0;
                 end
             end
         end else if (write_enable) begin
@@ -36,9 +37,9 @@ module chatgpt_neuron_network (
                 4'd5: FIRST_LAYER_WEIGHTS[2] <= data_in;
                 default: 
                     if (addr >= 4'd6 && addr <= 4'd14) begin
-                        i = (addr - 4'd6) / 3;
-                        j = (addr - 4'd6) % 3;
-                        SECOND_LAYER_WEIGHTS[i][j] <= data_in;
+                        idx6 = (addr - 4'd6) / 3;
+                        idx7 = (addr - 4'd6) % 3;
+                        SECOND_LAYER_WEIGHTS[idx6][idx7] <= data_in;
                     end
             endcase
         end
@@ -46,8 +47,8 @@ module chatgpt_neuron_network (
 
     // Compute input currents from spikes_in and FIRST_LAYER_WEIGHTS
     always @(*) begin
-        for (a = 0; a < 3; a = a + 1) begin
-            input_currents[a] = spikes_in[a] ? FIRST_LAYER_WEIGHTS[i] : 8'd0;
+        for (idx3 = 0; idx3 < 3; idx3 = idx3 + 1) begin
+            input_currents[idx3] = spikes_in[idx3] ? FIRST_LAYER_WEIGHTS[idx3] : 8'd0;
         end
     end
 
@@ -72,32 +73,31 @@ module chatgpt_neuron_network (
     // Logic to compute effective current for second layer neurons based on spikes and SECOND_LAYER_WEIGHTS
     reg [7:0] second_layer_currents[2:0];
     always @(posedge clk) begin
-        for (b = 0; b < 3; b = b + 1) begin
-            second_layer_currents[i] = 0;
-            for (c = 0; c < 3; c = c + 1) begin
-                if (first_layer_spikes[j]) begin
+        for (idx4 = 0; idx4 < 3; idx4 = idx4 + 1) begin
+            second_layer_currents[idx4] = 0;
+            for (idx5 = 0; idx5 < 3; idx5 = idx5 + 1) begin
+                if (first_layer_spikes[idx5]) begin
                     // Check for potential overflow
-                    if ((255 - second_layer_currents[b]) < SECOND_LAYER_WEIGHTS[c][b]) 
-                        second_layer_currents[b] = 255;  // Set to max value if overflow occurs
+                    if ((255 - second_layer_currents[idx4]) < SECOND_LAYER_WEIGHTS[idx5][idx4]) 
+                        second_layer_currents[idx4] = 255;  // Set to max value if overflow occurs
                     else
-                        second_layer_currents[b] = second_layer_currents[b] + SECOND_LAYER_WEIGHTS[c][b];
+                        second_layer_currents[idx4] = second_layer_currents[idx4] + SECOND_LAYER_WEIGHTS[idx5][idx4];
                 end
             end
         end
     end
 
     // Second layer of neurons
-    genvar kk;
     generate
-        for (kk = 0; kk < 3; kk = kk + 1) begin : SECOND_LAYER_GEN
+        for (k = 0; k < 3; k = k + 1) begin : SECOND_LAYER_GEN
             leaky_integrate_fire_neuron second_layer_inst (
                 .clk(clk),
                 .reset(reset),
                 .THRESHOLD(THRESHOLD),
                 .LEAK_RATE(LEAK_RATE),
                 .REFRAC_PERIOD(REFRAC_PERIOD),
-                .current(second_layer_currents[kk]),
-                .spike(spikes_out[kk])
+                .current(second_layer_currents[k]),
+                .spike(spikes_out[k])
             );
         end
     endgenerate
